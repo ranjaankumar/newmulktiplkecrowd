fec91351205162a1952d3f4f7fe80160d0909616
ranjaankumar <95124559+ranjaankumar@users.noreply.github.com>
2021-12-21 09:50:16 +0530

    Update mainfriend.py

--- mainfriend.py
+++ mainfriend.py
@@ -7,8 +7,8 @@ pat=[]
 
 
 vedantu = Webhook.from_url("https://discord.com/api/webhooks/922673692205539388/26QGscVp2hvE2mZUlOwATX_RVA-FZaLEgdI3PmUWtA-woECwWEbktlCA4DaIMj2swcz5",adapter=RequestsWebhookAdapter())
-hq = Webhook.from_url("https://discord.com/api/webhooks/922673692205539388/26QGscVp2hvE2mZUlOwATX_RVA-FZaLEgdI3PmUWtA-woECwWEbktlCA4DaIMj2swcz5",adapter=RequestsWebhookAdapter())
-swagbucks = Webhook.from_url("https://discord.com/api/webhooks/922673692205539388/26QGscVp2hvE2mZUlOwATX_RVA-FZaLEgdI3PmUWtA-woECwWEbktlCA4DaIMj2swcz5",adapter=RequestsWebhookAdapter())
+hq = Webhook.from_url("https://discord.com/api/webhooks/922697024380485662/CZmwkLkIMlXURtDtZFJhP8FdRFhsazrhsOCE4_80Hsd1vFM-ssyAJaFRqK-q3UKmP9mT",adapter=RequestsWebhookAdapter())
+swagbucks = Webhook.from_url("https://discord.com/api/webhooks/922698885003091989/ce3TsfFrOFxCS4Nj5nFzE0QrqNTVhsxenN2yLeyN8I8GpdcLqsCJhk0eQzAUL_JmJKSJ",adapter=RequestsWebhookAdapter())
 
 token = "OTA3NjM2MTIyNjgxNzUzNjIw.YYqEAg.XrBltIs25SwcS3axOqh2UkJ18aI"
 techds = ""
@@ -33,52 +33,52 @@ async def o(ctx):
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		vedantu.send(embed=embed)
 		
-	if ctx.message.channel.id == 921037549206663229:  #pheonix_vedantu
+	if ctx.message.channel.id == 918730322789662760:  #pheonix_vedantu
 		embed=discord.Embed(description="**Went With Option** :one:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		vedantu.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335:  #smart_vedantu
+	if ctx.message.channel.id == 920849694303850517:  #smart_vedantu
 		embed=discord.Embed(description="**Went With Option** :one:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		vedantu.send(embed=embed)
 		
-        if ctx.message.channel.id == 921037549206663229:  #stardom_swagbucks
+        if ctx.message.channel.id == 920855533077020765:  #stardom_swagbucks
 		embed=discord.Embed(description="**Went With Option** :one:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		swagbucks.send(embed=embed)
 		
-	if ctx.message.channel.id == 921037549206663229:  #daynite_swagbucks
+	if ctx.message.channel.id == 893400610957832202:  #daynite_swagbucks
 		embed=discord.Embed(description="**Went With Option** :one:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		swagbucks.send(embed=embed)
 		
-	if ctx.message.channel.id == 921037549206663229:  #smart_swagbucks
+	if ctx.message.channel.id == 919540946037772318:  #smart_swagbucks
 		embed=discord.Embed(description="**Went With Option** :one:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		swagbucks.send(embed=embed)
 		
-	if ctx.message.channel.id == 921037549206663229:  #pheonix_swagbucks
+	if ctx.message.channel.id == 922147520128368670:  #pheonix_swagbucks
 		embed=discord.Embed(description="**Went With Option** :one:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		swagbucks.send(embed=embed)
 		
-	if ctx.message.channel.id == 921037549206663229:  #stardom_hq
+	if ctx.message.channel.id == 918134314456191027:  #stardom_hq
 		embed=discord.Embed(description="**Went With Option** :one:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		hq.send(embed=embed)
 		
-	if ctx.message.channel.id == 921037549206663229:  #daynite_hq
+	if ctx.message.channel.id == 893400509803819028:  #daynite_hq
 		embed=discord.Embed(description="**Went With Option** :one:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		hq.send(embed=embed)
 		
-	if ctx.message.channel.id == 921037549206663229:  #pheonix_hq
+	if ctx.message.channel.id == 922023255005601875:  #pheonix_hq
 		embed=discord.Embed(description="**Went With Option** :one:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		hq.send(embed=embed)
 		
-	if ctx.message.channel.id == 921037549206663229:  #smart_hq
+	if ctx.message.channel.id == 920520092398256179:  #smart_hq
 		embed=discord.Embed(description="**Went With Option** :one:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		hq.send(embed=embed)
@@ -99,54 +99,54 @@ async def o(ctx):
 		vedantu.send(embed=embed)
 			
 		
-	if ctx.message.channel.id == 918012951791796335:  #pheonix_vedantu
+	if ctx.message.channel.id == 918730322789662760:  #pheonix_vedantu
 		embed=discord.Embed(description="**Went With Option** :two:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		vedantu.send(embed=embed)
 		
 		
-	if ctx.message.channel.id == 918012951791796335:  #smart_vedantu
+	if ctx.message.channel.id == 920849694303850517:  #smart_vedantu
 		embed=discord.Embed(description="**Went With Option** :two:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		vedantu.send(embed=embed)
 		
 		
-	if ctx.message.channel.id == 918012951791796335:  #stardom_swagbucks
+	if ctx.message.channel.id == 920855533077020765:  #stardom_swagbucks
 		embed=discord.Embed(description="**Went With Option** :two:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		swagbucks.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335:  #dayniye_swagbucks
+	if ctx.message.channel.id == 893400610957832202:  #dayniye_swagbucks
 		embed=discord.Embed(description="**Went With Option** :two:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		swagbucks.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335:  #pheonix_swagbucks
+	if ctx.message.channel.id == 922147520128368670:  #pheonix_swagbucks
 		embed=discord.Embed(description="**Went With Option** :two:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		swagbucks.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335:  #smart_swagbucks
+	if ctx.message.channel.id == 919540946037772318:  #smart_swagbucks
 		embed=discord.Embed(description="**Went With Option** :two:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		swagbucks.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335:  #stardom_hq
+	if ctx.message.channel.id == 918134314456191027:  #stardom_hq
 		embed=discord.Embed(description="**Went With Option** :two:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		hq.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335:  #dayniye_hq
+	if ctx.message.channel.id == 893400509803819028:  #dayniye_hq
 		embed=discord.Embed(description="**Went With Option** :two:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		hq.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335:  #pheonix_hq
+	if ctx.message.channel.id == 922023255005601875:  #pheonix_hq
 		embed=discord.Embed(description="**Went With Option** :two:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		hq.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335:  #smart_hq
+	if ctx.message.channel.id == 920520092398256179:  #smart_hq
 		embed=discord.Embed(description="**Went With Option** :two:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		hq.send(embed=embed)
@@ -166,52 +166,52 @@ async def o(ctx):
 		vedantu.send(embed=embed)
 		
 		
-	if ctx.message.channel.id == 918012951791796335: #pheonix_vedantu
+	if ctx.message.channel.id == 918730322789662760: #pheonix_vedantu
 		embed=discord.Embed(description="**Went With Option** :three:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		vedantu.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335: #smart_vedantu
+	if ctx.message.channel.id == 920849694303850517: #smart_vedantu
 		embed=discord.Embed(description="**Went With Option** :three:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		vedantu.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335: #stardom_swagbucks
+	if ctx.message.channel.id == 920855533077020765: #stardom_swagbucks
 		embed=discord.Embed(description="**Went With Option** :three:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		swagbucks.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335: #dayniye_swagbucks
+	if ctx.message.channel.id == 893400610957832202: #dayniye_swagbucks
 		embed=discord.Embed(description="**Went With Option** :three:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		swagbucks.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335: #pheonix_swagbucks
+	if ctx.message.channel.id == 922147520128368670: #pheonix_swagbucks
 		embed=discord.Embed(description="**Went With Option** :three:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		swagbucks.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335: #smart_swagbucks
+	if ctx.message.channel.id == 919540946037772318: #smart_swagbucks
 		embed=discord.Embed(description="**Went With Option** :three:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		swagbucks.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335: #stardom_hq
+	if ctx.message.channel.id == 918134314456191027: #stardom_hq
 		embed=discord.Embed(description="**Went With Option** :three:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		hq.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335: #dayniye_hq
+	if ctx.message.channel.id == 893400509803819028: #dayniye_hq
 		embed=discord.Embed(description="**Went With Option** :three:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		hq.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335: #pheonix_hq
+	if ctx.message.channel.id == 922023255005601875: #pheonix_hq
 		embed=discord.Embed(description="**Went With Option** :three:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		hq.send(embed=embed)
 		
-	if ctx.message.channel.id == 918012951791796335: #smart_hq
+	if ctx.message.channel.id == 920520092398256179: #smart_hq
 		embed=discord.Embed(description="**Went With Option** :three:",color=discord.Colour.random())
 		embed.set_author(name=f"{m.name}#{m.discriminator}", icon_url=m.avatar_url)
 		hq.send(embed=embed)
